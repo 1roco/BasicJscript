d8c8a7223b3b080144e207f9c4f822ccf99fbde0
1roco <roco.coding@gamil.com>
2022-06-24 17:12:25 +0100

    32 of 113 completed 24/6

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # BasicJscript
 Free code camp certification 300hrs!
 "Add index, jscript and style files, also boilerplate and css, js links"
+"Five out of 113 completed 23/6"
+"32 of 113 completed 23/6"
--- jscript.js
+++ jscript.js
@@ -1,1 +1,24 @@
 // BasicJscript Free code camp certification 300hrs!
+
+//Explore Differences Between the var and let Keywords
+// var camper = "James";
+// var camper = "David";
+// console.log(camper);
+// let camper = "James";
+// let camper = "David";
+// console.log(camper);
+
+// const FAV_PET = "Cats";
+// FAV_PET = "Dogs";
+
+//const myString = "FirstLine\n\t\\SecondLine\nThirdLine";
+//console.log(myString);
+
+// Setup
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+// Only change code below this line
+lastNameLength = lastName.length;
+console.log(lastNameLength);
+
