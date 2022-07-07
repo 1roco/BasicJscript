c6e718ff1d7738e81312c40c0a17bd43d12ed93e
1roco <roco.coding@gamil.com>
2022-07-07 15:55:45 +0100

    "Now at 56 of 113 complete"

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ Free code camp certification 300hrs!
 "Five out of 113 completed 23/6"
 "32 of 113 completed 24/6"
 "47 of 113 completed 27/6"
+"Previous result not saved but now at 56 of 113 complete 7/7"
 
--- jscript.js
+++ jscript.js
@@ -36,8 +36,62 @@
 // console.log(wordBlanks);
 
 // Setup
-const myArray = [["John", 23], ["cat", 2]];
-console.log(myArray);
+// const myArray = [["John", 23], ["cat", 2]];
+// console.log(myArray);
+// // Only change code below this line
+// const removedFromMyArray = myArray.pop();
+// console.log(removedFromMyArray);
+
+//local and global scope example
+// const loc = "fii";
+// function myTest() {
+//    // const loc = "foo";
+//     console.log(loc);
+//   }
+  
+//   myTest();
+//   //console.log(loc);
+  
+//Return or not to return and therefore undefined
+// let sum = 3;
+
+// function addSum(num) {
+//   sum = sum + num;
+//   //return(sum);
+// }
+
+// addSum(3);
+// console.log(sum);
+
+//More return or not return undefined
+// let sum = 0;
+
+// function addThree() {
+//   sum = sum + 3;
+//   return sum;
+// }
+
+// // Only change code below this line
+// function addFive() {
+// sum = sum + 5;
+// return sum;
+// }
+// // Only change code above this line
+
+// addThree();
+// console.log(sum);
+
+
+// addFive();
+// console.log(sum);
+
+//Return that returns?
+let processed = 0;
+
+function processArg(num) {
+  return (num + 3) / 5;
+}
+
 // Only change code below this line
-const removedFromMyArray = myArray.pop();
-console.log(removedFromMyArray);
+processed = processArg(7);
+console.log(processed);
